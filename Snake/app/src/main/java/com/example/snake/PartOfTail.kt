f10aebdab0a39bfcd66dffccbccb5017feb24772
UnicornBlood123 <gogulin.vova@mail.ru>
2021-12-01 12:13:55 +0300

    1task_1

--- Snake/app/src/main/java/com/example/snake/PartOfTail.kt
+++
@@ -1,8 +0,0 @@
-package com.example.snake
-
-import android.widget.ImageView
-
-data class PartOfTail (
-    val viewCoordinate: ViewCoordinate,
-    val imageView: ImageView
-)
--- Snake/app/src/main/java/com/example/snake/ViewCoordinate.kt
+++
@@ -1,7 +0,0 @@
-package com.example.snake
-
-data class ViewCoordinate (
-    val top:Int,
-    val left:Int
-)
-
